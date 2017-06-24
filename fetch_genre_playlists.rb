435c3f0e0281041bdad95c1984f75a8861053fb2
Milo Winningham <milo@winningham.net>
2017-06-24 12:24:37 -0700

    Fetch genre playlists

---
+++ fetch_genre_playlists.rb
@@ -0,0 +1,26 @@
+require "open-uri"
+require "nokogiri"
+require "json"
+require "byebug"
+
+html = open("http://everynoise.com/everynoise1d.cgi?scope=all")
+doc = Nokogiri::HTML(html)
+
+rows = doc.css("body > table > tr")
+
+SPOTIFY_ID = /[A-Za-z0-9]{22}/
+PLAYLIST_URI = /spotify:user:thesoundsofspotify:playlist:#{SPOTIFY_ID}/
+
+genres = rows.reduce({}) do |genres, row|
+  name = row.at("td.note:last").text
+  link = row.at("a.note")[:href]
+  uri = link[PLAYLIST_URI]
+  genres[name] = uri if uri
+  genres
+end
+
+if genres.size < 1500
+  raise "Too few genres found (#{genres.size} for #{rows.size} rows)"
+end
+
+puts JSON.pretty_generate(genres)
